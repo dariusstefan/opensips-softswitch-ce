241a9c69873475cab573fdc1dd16ecc2537991f5
Darius Stefan <darius.stefan@opensips.org>
2024-11-25 17:13:32 +0200

    Change tests directory's name

--- run-sipssert.sh
+++ run-sipssert.sh
@@ -7,6 +7,5 @@ docker run --rm -t \
     --env-file .env \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v "$(pwd)/${TESTS_DIR}":"$(pwd)/${TESTS_DIR}" \
-	-v "$(pwd)/run.yml":"$(pwd)/run.yml" \
 	-w "$(pwd)/${TESTS_DIR}" \
-	opensips/sipssert -c "$(pwd)/run.yml" . $@
+	opensips/sipssert . $@
