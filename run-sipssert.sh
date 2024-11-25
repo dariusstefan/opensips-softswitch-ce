9ac5fee905de63244c4f39488c2680203b6c48bf
Darius Stefan <darius.stefan@opensips.org>
2024-11-25 16:45:05 +0200

    Add run.yml for gh action

--- run-sipssert.sh
+++ run-sipssert.sh
@@ -7,5 +7,6 @@ docker run --rm -t \
     --env-file .env \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v "$(pwd)/${TESTS_DIR}":"$(pwd)/${TESTS_DIR}" \
+	-v "$(pwd)/run.yml":"$(pwd)/run.yml" \
 	-w "$(pwd)/${TESTS_DIR}" \
-	opensips/sipssert . $@
+	opensips/sipssert -c "$(pwd)/run.yml" . $@
